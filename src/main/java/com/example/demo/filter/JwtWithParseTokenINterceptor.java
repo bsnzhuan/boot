a5ebbe94b56a557c9114c41eb6c7ca84ccd2cf6b
bsnzhuan <wang_yh0320@163.com>
2020-05-02 19:52:10 +0800

    jwtParseToken

--- src/main/java/com/example/demo/filter/JwtWithParseTokenINterceptor.java
+++ src/main/java/com/example/demo/filter/JwtWithParseTokenINterceptor.java
@@ -1,17 +1,26 @@
 package com.example.demo.filter;
 
+import com.example.demo.anno.JwtParseToken;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 
 public class JwtWithParseTokenINterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        HandlerMethod method = (HandlerMethod)handler;
-        method.getMethodAnnotation();
+        HandlerMethod handlerMethod = (HandlerMethod)handler;
+        Method method = handlerMethod.getMethod();
+        if(method.isAnnotationPresent(JwtParseToken.class)){
+            JwtParseToken parseToken = method.getAnnotation(JwtParseToken.class);
+            if(parseToken.required()){
+                return true;
+            }
+        }
+        
         return false;
     }
 
